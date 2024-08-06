619e07954dcbf2dafef6e8dd54786fb2d510ba96
Baibhav <baibhavk6@gmail.com>
2024-08-06 10:46:57 +0530

    ..

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ db.connect((err) => {
     console.log('Connected to MySQL DB');
 });
 
-
 app.post('/api/signup', async (req, res) => {
     try {
         const { email, username, password } = req.body;
@@ -50,34 +49,6 @@ app.post('/api/signup', async (req, res) => {
     }
 });
 
-app.post('/api/login', async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      const getUserQuery = 'SELECT * FROM users WHERE username = ?';
-      db.query(getUserQuery, [username], async (err, rows) => {
-        if (err || rows.length === 0) {
-          res.status(401).json({ status: 'User not found', status_code: 401 });
-        } else {
-          const user = rows[0];
-          const passwordMatch = await bcrypt.compare(password.trim(), user.password);
-          if (passwordMatch) {
-            const token = jwt.sign({ user_id: user.user_id }, 'key', { expiresIn: '1h' });
-            res.status(200).json({
-              status: 'Login successful',
-              status_code: 200,
-              user_id: user.user_id,
-              access_token: token,
-            });
-          } else {
-            res.status(401).json({ status: 'Incorrect username/password provided. Please retry', status_code: 401 });
-          }
-        }
-      });
-    } catch (error) {
-      console.error('Error during login:', error);
-      res.status(500).json({ message: 'Error during login' });
-    }
-  });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
