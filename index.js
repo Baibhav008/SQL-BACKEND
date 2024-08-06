8b1338badf2425cf3882fa52ed345269994501b3
Baibhav <baibhavk6@gmail.com>
2024-08-06 10:05:24 +0530

    login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 
 const app = express();
@@ -49,7 +50,41 @@ app.post('/api/signup', async (req, res) => {
     }
 });
 
+app.post('/api/login', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const getUserQuery = 'SELECT * FROM users WHERE username = ?';
+      db.query(getUserQuery, [username], async (err, rows) => {
+        if (err || rows.length === 0) {
+          res.status(401).json({ status: 'User not found', status_code: 401 });
+        } else {
+          const user = rows[0];
+          console.log('Stored password:', user.password);
+          console.log('Provided password:', password);
+          const passwordMatch = await bcrypt.compare(password.trim(), user.password);
+          console.log('Password match:', passwordMatch);
+  
+          if (passwordMatch) {
+            const token = jwt.sign({ user_id: user.user_id }, 'key', { expiresIn: '1h' });
+            res.status(200).json({
+              status: 'Login successful',
+              status_code: 200,
+              user_id: user.user_id,
+              access_token: token,
+            });
+          } else {
+            res.status(401).json({ status: 'Incorrect username/password provided. Please retry', status_code: 401 });
+          }
+        }
+      });
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ message: 'Error during login' });
+    }
+  });
 
+
+  
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
